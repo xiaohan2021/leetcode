08d316030ac8fe9b95b6ffc442d0a42c54770408
xiaohan2021 <hankang307@bupt.edu.cn>
2021-02-24 17:45:20 +0800

    第一遍刷题解

--- src/com/offer/replaceSpace/ReplaceSpace.java
+++ src/com/offer/replaceSpace/ReplaceSpace.java
@@ -1,25 +1,41 @@
 package com.offer.replaceSpace;
 
-import javax.xml.transform.Source;
-import java.util.Arrays;
-
 /**
- * @Description:
+ * @Description: 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
  * @Author: 小韩同学
  * @Date: 2020/11/18
  */
 public class ReplaceSpace {
     public static void main(String[] args) {
-        String s = "We are happy.";
-        replaceSpace(s);
-        System.out.println(s);
+        String s = "    ";
+        String res = replaceSpace(s);
+        System.out.println(res);
     }
 
     public static String replaceSpace(String s) {
-        char[] arr = s.toCharArray();
+        /**
+         * 我自己写的方法不可以测试字符串为全空格的情况！！！
+        String res = "";
+        String[] arr = s.split(" ");
         for (int i = 0; i < arr.length; i++) {
-
+            if (i != arr.length - 1) {
+                res = res + arr[i] + "%20";
+            } else {
+                res = res + arr[i];
+            }
         }
-        return s;
+        return res;
+         */
+        StringBuilder res = new StringBuilder();
+        for(Character c : s.toCharArray())
+        {
+            if(c == ' ') {
+                res.append("%20");
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
     }
+
 }
